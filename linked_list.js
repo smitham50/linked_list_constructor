6aa4dd121ec520b62f914b0ed143388935a163d6
Aaron Smith <smitham50@gmail.com>
2019-10-18 15:32:41 -0400

    pushdown stack implementation

--- linked_list.js
+++ linked_list.js
@@ -4,7 +4,8 @@ class LinkedList {
     this.head = null;
     this.size = 0;
   }
-  //FIFO queue, add to tail, remove from head
+  //FIFO QUEUE 
+  //add to tail 
   enqueue(element) {
     let node = new Node(element);
     let current;
@@ -22,14 +23,31 @@ class LinkedList {
 
     this.size++;
   };
+  //remove from head
   dequeue() {
     this.head = this.head.next;
     this.size--;
   }
+  //PUSHDOWN STACK
+  //add to head
+  push(element) {
+    let prevHead = this.head;
+    this.head = new Node(element);
+    this.head.next = prevHead;
+    this.size++;
+  }
+  //remove from head
+  pop() {
+    let element = this.head.element;
+    this.head = this.head.next;
+    this.size--;
+    return element;
+  }
 }
 
 class Node {
-  constructor(element) {
+  constructor(element) 
+  {
     this.element = element;
     this.next = null;
   }
@@ -42,4 +60,8 @@ list.enqueue("third");
 console.log(list, list.head, list.head.next, list.head.next.next);
 list.dequeue();
 list.enqueue("first");
+console.log(list, list.head, list.head.next, list.head.next.next);
+list.push("first");
+list.push("fourth");
+console.log(list.pop());
 console.log(list, list.head, list.head.next, list.head.next.next);
